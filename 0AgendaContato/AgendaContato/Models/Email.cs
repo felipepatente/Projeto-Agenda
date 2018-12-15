2b918db390fab3853b0ae23338608b3a7f06bd00
felipepatente <felipepatente@yahoo.com.br>
2018-12-15 19:54:15 -0200

    Colocando as anotações necessarias

--- 0AgendaContato/AgendaContato/Models/Email.cs
+++ 0AgendaContato/AgendaContato/Models/Email.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace AgendaContato.Models
     {
         public int Id { get; set; }
 
+        [EmailAddress, Required]
         public String EmailContato { get; set; }
 
         public int NomeId { get; set; }
--- 0AgendaContato/AgendaContato/Models/Telefone.cs
+++ 0AgendaContato/AgendaContato/Models/Telefone.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace AgendaContato.Models
     {
         public int Id { get; set; }
 
+        [Phone, Required]
         public String Numero { get; set; }
 
         public int NomeId { get; set; }
