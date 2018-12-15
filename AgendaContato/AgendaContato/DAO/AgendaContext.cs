ecd83ce82253c5d07f27f8e1c6014bc20ab681b5
felipepatente <felipepatente@yahoo.com.br>
2018-12-15 19:39:14 -0200

    Criando o contexto

---
+++ AgendaContato/AgendaContato/DAO/AgendaContext.cs
@@ -0,0 +1,19 @@
+﻿using AgendaContato.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AgendaContato.DAO
+{
+    public class AgendaContext : DbContext
+    {
+        public DbSet<Email> Emails { get; set; }
+
+        public DbSet<Nome> Nomes { get; set; }
+
+        public DbSet<Telefone> Telefones { get; set; }
+        
+    }
+}
